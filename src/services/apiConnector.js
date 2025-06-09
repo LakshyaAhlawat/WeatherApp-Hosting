14e7230a12f9a037deda95c743d3b8ff37993915
LakshyaAhlawat <ahlawat.lakshya.2004@gmail.com>
2025-06-10 03:49:08 +0530

    changed in apiconnector

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -33,7 +33,7 @@
 //   return res.json();
 // };
 
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = $`process.env.REACT_APP_API_BASE`;
 
 // Signup
 export const signup = async (email, username, password) => {
